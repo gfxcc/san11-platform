99ed045fe7df527627fa7e01d2b6dab74f640c91
yong <yong_stevens@outlook.com>
2021-03-07 11:17:25 -0500

    restructure layout

--- back-end/src/lib/db_util.py
+++ back-end/src/lib/db_util.py
@@ -1,8 +1,10 @@
+import os
 import psycopg2
+
 
 from typing import List, Tuple, Dict
 
-db_conn = psycopg2.connect(host="db", database="san11-platform-db", user="postgres", password="example")
+db_conn = psycopg2.connect(host="db", database=os.environ['DB_NAME'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'])
     
 
 def run_sql_with_param(sql: str, param: Dict) -> List[Tuple]:
