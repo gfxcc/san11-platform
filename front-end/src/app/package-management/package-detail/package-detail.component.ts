5df3cbf241777484c0c3b20340b5dd5efad816f1
yong <yong_stevens@outlook.com>
2021-04-18 19:16:01 -0400

    disable link in descEditor

--- front-end/src/app/package-management/package-detail/package-detail.component.ts
+++ front-end/src/app/package-management/package-detail/package-detail.component.ts
@@ -105,7 +105,7 @@ export class PackageDetailComponent implements OnInit {
         '|',
         'bold',
         'italic',
-        'link',
+        // 'link',
         'bulletedList',
         'numberedList',
         '|',
