8710805cf057b08cdfe7e6f366780cd8d7c09bf3
yong <yong_stevens@outlook.com>
2021-04-24 18:08:10 -0400

    update @ system

--- front-end/src/app/package-management/package-detail/package-detail.component.ts
+++ front-end/src/app/package-management/package-detail/package-detail.component.ts
@@ -24,7 +24,7 @@ import { UserDetailComponent } from "../../account-management/user-detail/user-d
 
 import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
-import { isAdmin, getUsernameFeeds } from "../../utils/user_util";
+import { isAdmin, getUsernameFeeds, getUserUrl } from "../../utils/user_util";
 import { increment } from '../../utils/number_util';
 import { getPackageUrl } from "../../utils/package_util";
 
@@ -179,7 +179,7 @@ export class PackageDetailComponent implements OnInit {
           id: `@${user.username}`,
           userId: user.userId,
           username: user.username,
-          link: getFullUrl(`users/${user.userId}`),
+          link: `${GlobalConstants.serverIp}/${getUserUrl(user)}`,
           userAvatar: getFullUrl(user.imageUrl)
         }));
         console.log(this.userFeeds);
@@ -214,6 +214,66 @@ export class PackageDetailComponent implements OnInit {
       );
     }
   }
+
+  MentionCustomization(editor) {
+    // The upcast converter will convert view <a class="mention" href="" data-user-id="">
+    // elements to the model 'mention' text attribute.
+    editor.conversion.for('upcast').elementToAttribute({
+      view: {
+        name: 'a',
+        key: 'data-mention',
+        classes: 'mention',
+        attributes: {
+          href: true,
+          'data-user-id': true
+        }
+      },
+      model: {
+        key: 'mention',
+        value: viewItem => {
+          // The mention feature expects that the mention attribute value
+          // in the model is a plain object with a set of additional attributes.
+          // In order to create a proper object use the toMentionAttribute() helper method:
+          const mentionAttribute = editor.plugins.get('Mention').toMentionAttribute(viewItem, {
+            // Add any other properties that you need.
+            link: viewItem.getAttribute('href'),
+            userId: viewItem.getAttribute('data-user-id')
+          });
+
+          return mentionAttribute;
+        }
+      },
+      converterPriority: 'high'
+    });
+
+    // Downcast the model 'mention' text attribute to a view <a> element.
+    editor.conversion.for('downcast').attributeToElement({
+      model: 'mention',
+      view: (modelAttributeValue, { writer }) => {
+        // Do not convert empty attributes (lack of value means no mention).
+        if (!modelAttributeValue) {
+          return;
+        }
+
+        return writer.createAttributeElement('a', {
+          class: 'mention',
+          'data-mention': modelAttributeValue.id,
+          'data-user-id': modelAttributeValue.userId,
+          'href': modelAttributeValue.link
+        }, {
+          // Make mention attribute to be wrapped by other attribute elements.
+          priority: 20,
+          // Prevent merging mentions together.
+          id: modelAttributeValue.uid
+        });
+      },
+      converterPriority: 'high'
+    });
+  }
+
+  //
+  // Editor-END
+  //
 
   onUpdateTitle() {
     const updatedPackageName = this.packageNameElement.nativeElement.innerHTML;
