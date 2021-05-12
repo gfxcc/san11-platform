6c821c1e812bccf6d85c29b52ccffafcd41b3017
yong <yong_stevens@outlook.com>
2021-05-11 23:09:40 -0400

    add retry on gcs operations

--- back-end/src/lib/gcs.py
+++ back-end/src/lib/gcs.py
@@ -1,5 +1,6 @@
 import logging, os
 from google.cloud import storage
+from retry import retry
 
 
 logger = logging.getLogger(os.path.basename(__file__))
@@ -10,7 +11,11 @@ CANONICAL_BUCKET = 'san11-resources'
 # Limits
 PACKAGE_LIMIT_GB = 10
 
+RETRY_ATTEMPT = 3 # times
+RETRY_DELAY = 2 # seconds 
 
+
+@retry(Exception, tries=RETRY_ATTEMPT, delay=RETRY_DELAY)
 def move_file(src_bucket_name: str, src_filename: str, dest_bucket_name: str, dest_filename: str) -> None:
     storage_client = storage.Client()
 
@@ -27,6 +32,7 @@ def move_file(src_bucket_name: str, src_filename: str, dest_bucket_name: str, de
     logger.debug(f'({src_filename}) is deleted from bucket {src_bucket_name}')
 
 
+@retry(Exception, tries=RETRY_ATTEMPT, delay=RETRY_DELAY)
 def delete_file(bucket_name: str, filename: str) -> None:
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -34,10 +40,7 @@ def delete_file(bucket_name: str, filename: str) -> None:
     logger.debug(f'({filename}) is deleted from bucket {bucket_name}')
 
 
-def delete_canonical_resource(url: str) -> None:
-    delete_file(CANONICAL_BUCKET, url)
-
-
+@retry(Exception, tries=RETRY_ATTEMPT, delay=RETRY_DELAY)
 def disk_usage_under(prefix: str) -> int:
     '''
     Return size of all resources with give prefix under CANONICAL_BUCKET
@@ -47,8 +50,13 @@ def disk_usage_under(prefix: str) -> int:
     return sum(blob.size for blob in blobs)
 
 
+@retry(Exception, tries=RETRY_ATTEMPT, delay=RETRY_DELAY)
 def get_file_size(bucket_name: str, filename: str) -> int:
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.get_blob(filename)
     return blob.size
+
+
+def delete_canonical_resource(url: str) -> None:
+    delete_file(CANONICAL_BUCKET, url)
