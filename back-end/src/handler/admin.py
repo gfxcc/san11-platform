f4b850ff99611dc265e876da734d9cf0341ff1b7
yong <yong_stevens@outlook.com>
2021-05-17 22:17:14 -0400

    enrich admin message

--- back-end/src/handler/admin.py
+++ back-end/src/handler/admin.py
@@ -24,6 +24,10 @@ class AdminHandler:
         message['new-users'] = list(map(lambda fields: ', '.join(map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
         sql = 'SELECT name, create_time FROM packages ORDER BY create_time desc limit 10'
         message['new-packages'] = list(map(lambda fields: ', '.join(map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
+        sql = 'SELECT parent, text, create_time FROM comments ORDER BY create_time desc limit 10'
+        message['new-comments'] = list(map(lambda fields: ', '.join(map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
+        sql = 'SELECT comment_id, text, create_time FROM replies ORDER BY create_time desc limit 10'
+        message['new-replies'] = list(map(lambda fields: ', '.join(map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
         return san11_platform_pb2.AdminMessage(
             message=json.dumps(message, indent=4, ensure_ascii=False)
         )
