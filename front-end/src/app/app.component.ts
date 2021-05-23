d7b4ef6f2e48852236515f79b539a4f6b9bfca39
yong <yong_stevens@outlook.com>
2021-05-23 15:48:48 -0400

    ui adjustment

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -21,109 +21,32 @@ import { clearUser, loadUser, signedIn, isAdmin } from './utils/user_util';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
-  categories = GlobalConstants.categories;
-  webModules = GlobalConstants.webModules;
-  adminModules = GlobalConstants.adminModules;
-
   sideBarOpen = true;
 
-  title = 'san11-platform';
-
-  selectedCategory = '1';
-
-  user: User;
-  hideUserImage = true;
-  userImage: string;
-
-  tagGroups = [];
-
-  // tagGroups = [{
-  //   groupName: 'SIRE 版本',
-  //   tags: ['SIRE 2', 'SIRE 1']
-  // }, {
-  //   groupName: '类型',
-  //   tags: ['通用', '战法', '特技', '计略', '部队', '战略', '地形', 'AI']
-  // }];
-
   constructor(
-    private notificationService: NotificationService,
-    private san11pkService: San11PlatformServiceService,
     private dialog: MatDialog,
-    private _eventEmiter: EventEmiterService,
     public router: Router) {
   }
 
   ngOnInit(): void {
-    this.loadTags();
   }
 
-  ngAfterViewInit(): void {
-  }
 
   sideBarToggler(event) {
     this.sideBarOpen = !this.sideBarOpen;
   }
 
-  createTag(event) {
-    const newTagName = event.target.value;
-    this.san11pkService.createTag(new CreateTagRequest({
-      tag: new Tag({
-        categoryId: this.selectedCategory,
-        name: newTagName
-      })
-    })).subscribe(
-      tag => {
-        this.loadTags();
-      },
-      error => {
-        this.notificationService.warn(`创建标签 失败: ${error.statusMessage}`);
-      }
-    );
-  }
-
-  loadTags() {
-    this.san11pkService.listTags(new ListTagsRequest({ categoryId: this.selectedCategory })).subscribe(
-      resp => {
-        if (resp.tags.length === 0) {
-          this.tagGroups = [];
-        } else {
-          this.tagGroups = [{
-            groupName: '类型',
-            tags: resp.tags
-          }];
-        }
-      },
-      error => {
-        this.notificationService.warn('无法获取 标签:' + error.statusMessage);
-      }
-    );
-  }
-
-
-  username() {
-    return localStorage.getItem('username');
-  }
-  userId() {
-    return localStorage.getItem('userId');
-  }
-  isAdmin() {
-    return isAdmin();
-  }
-
-
   onActivate(elementRef) {
-    this._eventEmiter.dataStr.subscribe((data: ComponentMessage) => {
-      if (data.categoryId != undefined) {
-        setTimeout(() => {
-          if (this.selectedCategory === data.categoryId) {
-            return;
-          }
-          this.selectedCategory = data.categoryId;
-          this.loadTags();
-        });
-      }
-    });
+    // this._eventEmiter.dataStr.subscribe((data: ComponentMessage) => {
+    //   if (data.categoryId != undefined) {
+    //     setTimeout(() => {
+    //       if (this.selectedCategory === data.categoryId) {
+    //         return;
+    //       }
+    //       this.selectedCategory = data.categoryId;
+    //       this.loadTags();
+    //     });
+    //   }
+    // });
   }
 }
