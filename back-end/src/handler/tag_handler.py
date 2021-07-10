17a47bd49c87d055288b23b3b3b92cc820b6b1ad
yong <yong_stevens@outlook.com>
2021-07-09 22:11:05 -0400

    back-end refactoring

--- back-end/src/handler/tag_handler.py
+++ back-end/src/handler/tag_handler.py
@@ -13,7 +13,6 @@ logger = logging.getLogger(os.path.basename(__file__))
 
 class TagHandler:
     def create_tag(self, request, context):
-        logger.info(f'In CreateTag: {request.tag.name}')
         auth = Authenticator.from_context(context=context)
         assert auth.isAdmin()
         tag = Tag.from_pb(request.tag)
@@ -21,14 +20,12 @@ class TagHandler:
         return tag.to_pb()
     
     def delete_tag(self, request, context):
-        logger.info('In DeleteTag')
         auth = Authenticator.from_context(context=context)
         assert auth.isAdmin()
         Tag.from_id(request.tag_id).delete(user_id=auth.session.user.user_id)
         return san11_platform_pb2.Empty()
 
     def list_tags(self, request, context):
-        logger.info(f'In ListTags: category_id: {request.category_id}')
         return san11_platform_pb2.ListTagsResponse(
             tags=[tag.to_pb() for tag in Tag.list(page_size=0,
                                                   page_token='',
