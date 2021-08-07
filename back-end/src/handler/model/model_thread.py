8e80d5f403963eff83e3d38bceda4a20e2bc3d77
yong <yong_stevens@outlook.com>
2021-08-07 11:28:10 -0400

    wip

---
+++ back-end/src/handler/model/model_thread.py
@@ -0,0 +1,50 @@
+import attr
+import datetime
+
+from .base import ModelBase, Attrib, InitModel
+from ..protos import san11_platform_pb2 as pb
+from ..util.time_util import get_now
+
+
+@InitModel(
+    db_table='threads',
+    proto_class=pb.Thread,
+)
+@attr.s
+class Thread(ModelBase):
+    # Resource name. It is `{parent}/articles/{article_id}`
+    # E.g. `articles/12345`
+    name = Attrib(
+        type=str,
+    )
+    subject = Attrib(
+        type=str,
+    )
+    content = Attrib(
+        type=str,
+    )
+    author_id = Attrib(
+        type=int,
+    )
+    state = Attrib(
+        type=int,
+    )
+    tags = Attrib(
+        type=str,
+        repeated=True,
+    )
+    view_count = Attrib(
+        type=int,
+    )
+    like_count = Attrib(
+        type=int,
+    )
+    create_time = Attrib(
+        type=datetime.datetime,
+        default=get_now(),
+    )
+    update_time = Attrib(
+        type=datetime.datetime,
+        default=get_now(),
+    )
+
