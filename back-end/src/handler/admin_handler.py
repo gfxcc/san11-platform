31b7af38fff4d472ed42c589b4d3c700b24b5853
nan <nliu307@gatech.edu>
2022-01-17 14:31:54 -0800

    fix admin_handler

--- back-end/src/handler/admin_handler.py
+++ back-end/src/handler/admin_handler.py
@@ -14,9 +14,6 @@ class AdminHandler:
         sql = 'SELECT username, create_timestamp FROM users ORDER BY create_timestamp desc limit 10'
         message['new-users'] = list(map(lambda fields: ', '.join(
             map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
-        sql = 'SELECT name, create_time FROM packages ORDER BY create_time desc limit 10'
-        message['new-packages'] = list(map(lambda fields: ', '.join(
-            map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
         return san11_platform_pb2.AdminMessage(
             message=json.dumps(message, indent=4, ensure_ascii=False)
         )
