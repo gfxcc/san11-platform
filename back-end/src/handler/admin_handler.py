317759510d9f198454a5d23023e7a77ed5cd9617
yong <ycao181@gmail.com>
2022-01-25 09:35:41 -0800

    bug fix

--- back-end/src/handler/admin_handler.py
+++ back-end/src/handler/admin_handler.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(os.path.basename(__file__))
 class AdminHandler:
     def get_admin_message(self, request, context):
         message = {}
-        sql = 'SELECT username, create_timestamp FROM users ORDER BY create_timestamp desc limit 10'
-        message['new-users'] = list(map(lambda fields: ', '.join(
-            map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
+        # sql = 'SELECT username, create_timestamp FROM users ORDER BY create_timestamp desc limit 10'
+        # message['new-users'] = list(map(lambda fields: ', '.join(
+        #     map(str, fields)), run_sql_with_param_and_fetch_all(sql, {})))
         return san11_platform_pb2.AdminMessage(
             message=json.dumps(message, indent=4, ensure_ascii=False)
         )
