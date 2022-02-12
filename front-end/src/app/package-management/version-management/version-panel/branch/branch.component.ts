0681e78373728a0d425a03ee3ae7801e222146de
yong <ycao181@gmail.com>
2022-02-12 15:03:12 -0800

    fix download issue

--- front-end/src/app/package-management/version-management/version-panel/branch/branch.component.ts
+++ front-end/src/app/package-management/version-management/version-panel/branch/branch.component.ts
@@ -1,8 +1,19 @@
-import { Component, Input, ViewChild } from "@angular/core";
+import { HttpEventType } from "@angular/common/http";
+import { Component, EventEmitter, Input, Output, ViewChild } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatTableDataSource } from "@angular/material/table";
-import { version2str } from "src/app/utils/binary_util";
-import { Binary } from "src/proto/san11-platform.pb";
+import { saveAs } from 'file-saver';
+import { Subscription } from "rxjs";
+import { TextDialogComponent } from "src/app/common/components/text-dialog/text-dialog.component";
+import { GlobalConstants } from "src/app/common/global-constants";
+import { NotificationService } from "src/app/common/notification.service";
+import { DownloadService } from "src/app/service/download.service";
+import { San11PlatformServiceService } from "src/app/service/san11-platform-service.service";
+import { getBinaryFilename, version2str } from "src/app/utils/binary_util";
+import { increment } from "src/app/utils/number_util";
+import { signedIn } from "src/app/utils/user_util";
+import { Binary, DownloadBinaryRequest, FieldMask, Package, UpdateBinaryRequest } from "src/proto/san11-platform.pb";
 
 
 export interface Branch {
@@ -16,19 +27,41 @@ export interface Branch {
     styleUrls: ['./branch.css']
 })
 export class BranchComponent {
+    @Input() package: Package;
     @Input() branch: Branch;
     @Input() editorPermission: boolean;
+    @Output() updateDownloadProgress = new EventEmitter();
+    @Output() reload = new EventEmitter();
     @ViewChild(MatPaginator) paginator: MatPaginator;
 
     displayedColumns: string[] = ['version', 'createTime', 'size', 'downloadCount', 'actions'];
     dataSource: MatTableDataSource<Binary>;
 
-    constructor() {
+
+    binaryOnDownload: Binary;
+    downloadSub: Subscription;
+    downloadProgress: number;
+    startTime: any;
+    endTime: any;
+    currTime: any;
+    prevTime: any;
+    SPEED_UPDATE_DELTA_RATE = 0.2;
+    bytesReceied: number = 0;
+    oldbytes: number = 0;
+    unit: string = "Mbps";
+    speed: number = 0;
+
+
+    constructor(
+        private dialog: MatDialog,
+        private san11pkService: San11PlatformServiceService,
+        private notificationService: NotificationService,
+        private downloads: DownloadService,
+    ) {
     }
 
     ngOnInit(): void {
         this.dataSource = new MatTableDataSource(this.branch.binaries);
-        // this.dataSource.paginator = this.paginator;
     }
 
     ngAfterViewInit() {
@@ -37,5 +70,151 @@ export class BranchComponent {
 
     getVersionStr(binary: Binary): string {
         return version2str(binary.version);
+    }
+
+
+    onDescription(binary: Binary) {
+        this.dialog.open(TextDialogComponent, {
+            data: {
+                title: "更新日志",
+                content: binary.description,
+                editable: this.editorPermission,
+            }
+        }).afterClosed().subscribe(
+            data => {
+                if (this.editorPermission && data && data.data != binary.description) {
+                    const request = new UpdateBinaryRequest({
+                        binary: new Binary({
+                            name: binary.name,
+                            description: data.data,
+                        }),
+                        updateMask: new FieldMask({
+                            paths: ['description']
+                        })
+                    });
+
+                    this.san11pkService.updateBinary(request).subscribe(
+                        binary => {
+                            this.notificationService.success('更新成功');
+                        },
+                        error => {
+                            this.notificationService.warn(`更新失败: ${error.statusMessage}`)
+                        }
+                    );
+                }
+            }
+        );
+    }
+
+    onDownload(binary: Binary) {
+        if (!signedIn()) {
+            this.notificationService.warn('请登录');
+            return;
+        }
+
+        // if (this.downloadSub != undefined) {
+        //     this.notificationService.warn(`下载中 ${this.downloadProgress}%... `);
+        //     return;
+        // }
+
+        this.downloadProgress = 0;
+        this.binaryOnDownload = binary;
+        this.san11pkService.downloadBinary(new DownloadBinaryRequest({
+            name: this.binaryOnDownload.name
+        })).subscribe(
+            binary => {
+                const fileUrl = GlobalConstants.fileServerUrl + '/' + binary.file.uri;
+                const filename = getBinaryFilename(this.package, binary);
+
+                this.prevTime = new Date().getTime();
+                this.oldbytes = 0;
+
+                this.downloadSub = this.downloads.download(fileUrl, filename).subscribe(
+                    result => {
+                        if (result.type === HttpEventType.DownloadProgress) {
+                            const percentDone = Math.round(100 * result.loaded / result.total);
+                            this.downloadProgress = percentDone;
+
+                            this.bytesReceied = result.loaded / 1000000;
+                            this.currTime = new Date().getTime();
+                            let speed =
+                                (this.bytesReceied - this.oldbytes) /
+                                ((this.currTime - this.prevTime) / 1000); // mb/second
+                            if (speed < 1) {
+                                this.unit = "KB/S";
+                                speed *= 1000;
+                            } else this.unit = "MB/S";
+
+                            // Use Math.max(this.speed, 1) to handle this.speend == 0
+                            if (Math.abs(this.speed - speed) / Math.max(this.speed, 1) > this.SPEED_UPDATE_DELTA_RATE) {
+                                this.speed = speed;
+                            }
+                            this.prevTime = this.currTime;
+                            this.oldbytes = this.bytesReceied;
+
+                            this.updateDownloadProgress.emit({
+                                progress: percentDone,
+                                speed: this.speed,
+                                unit: this.unit,
+                            });
+                        }
+                        if (result.type === HttpEventType.Response) {
+                            this.updateDownloadProgress.emit({
+                                progress: 100,
+                                speed: this.speed,
+                                unit: this.unit,
+                            });
+
+                            this.downloadSub = undefined;
+                            saveAs(result.body, filename);
+                            this.binaryOnDownload.downloadCount = increment(this.binaryOnDownload.downloadCount);
+                        }
+                    },
+                    error => {
+                        this.downloadSub = undefined;
+                        this.notificationService.warn('下载失败: ' + error.name);
+                    }
+                );
+            },
+            error => {
+                this.notificationService.warn('下载失败' + error.statusMessage);
+            }
+        );
+    }
+
+    onDownloadMethod(binary: Binary) {
+        this.dialog.open(TextDialogComponent, {
+            data: {
+                title: '下载方式',
+                content: binary.downloadMethod
+            }
+        });
+        binary.downloadCount = increment(binary.downloadCount);
+        this.san11pkService.downloadBinary(new DownloadBinaryRequest({
+            name: binary.name
+        })).subscribe();
+    }
+
+
+    onOffload(binary: Binary) {
+        if (!confirm('确定要 ' + version2str(binary.version) + ' 的文件资源吗? (只清除资源，版本将会保留)')) {
+            return;
+        }
+        this.san11pkService.updateBinary(new UpdateBinaryRequest({
+            binary: new Binary({
+                name: binary.name,
+            }),
+            updateMask: new FieldMask({
+                paths: ['file']
+            })
+        })).subscribe(
+            resp => {
+                this.notificationService.success('卸载成功');
+                this.reload.emit();
+            },
+            error => {
+                this.notificationService.warn('卸载失败:' + error.statusMessage);
+            }
+        );
     }
 }
--- front-end/src/app/package-management/version-management/version-panel/version-panel.component.ts
+++ front-end/src/app/package-management/version-management/version-panel/version-panel.component.ts
@@ -1,21 +1,14 @@
-import { HttpEventType } from "@angular/common/http";
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from "@angular/material/paginator";
-import { saveAs } from 'file-saver';
-import { Subscription } from 'rxjs';
-import { Binary, DeleteBinaryRequest, DownloadBinaryRequest, FieldMask, ListBinariesRequest, ListBinariesResponse, Package, UpdateBinaryRequest, Version as PbVersion } from "../../../../proto/san11-platform.pb";
-import { TextDialogComponent } from "../../../common/components/text-dialog/text-dialog.component";
-import { GlobalConstants } from "../../../common/global-constants";
+import { Binary, DeleteBinaryRequest, ListBinariesRequest, ListBinariesResponse, Package, Version as PbVersion } from "../../../../proto/san11-platform.pb";
 import { NotificationService } from "../../../common/notification.service";
 import { BinaryService } from "../../../service/binary-service";
-import { DownloadService } from "../../../service/download.service";
 import { San11PlatformServiceService } from "../../../service/san11-platform-service.service";
-import { getBinaryFilename, version2str } from "../../../utils/binary_util";
-import { increment } from '../../../utils/number_util';
+import { version2str } from "../../../utils/binary_util";
 import { getCategoryId, getPackageUrl } from "../../../utils/package_util";
 import { getAcceptFileType } from "../../../utils/resrouce_util";
-import { isAdmin, signedIn } from '../../../utils/user_util';
+import { isAdmin } from '../../../utils/user_util';
 import { CreateNewVersionComponent } from "../create-new-version/create-new-version.component";
 import { Branch } from "./branch/branch.component";
 
@@ -29,32 +22,21 @@ export class VersionPanelComponent implements OnInit {
   @Output() downloadEvent = new EventEmitter();
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  displayedColumns: string[] = ['version', 'createTime', 'size', 'downloadCount', 'actions'];
-  binaryOnDownload: Binary;
-
-  downloadProgress: Number;
   downloadProgressBar = false;
+  downloadProgress: number;
+  unit: string = "Mbps";
+  speed: number = 0;
+
+  displayedColumns: string[] = ['version', 'createTime', 'size', 'downloadCount', 'actions'];
+
   branchs: Branch[];
   tabSelectedIndex = 0;
-
-  downloadSub: Subscription;
-
-  startTime: any;
-  endTime: any;
-  currTime: any;
-  prevTime: any;
-  speed: number = 0;
-  SPEED_UPDATE_DELTA_RATE = 0.2;
-  bytesReceied: number = 0;
-  oldbytes: number = 0;
-  unit: string = "Mbps";
 
   constructor(
     private san11pkService: San11PlatformServiceService,
     private notificationService: NotificationService,
     private binaryService: BinaryService,
     private dialog: MatDialog,
-    private downloads: DownloadService,
   ) {
   }
 
@@ -82,6 +64,7 @@ export class VersionPanelComponent implements OnInit {
     );
   }
 
+  // Create a new binary
   onUpdate() {
     const selectedTab = this.branchs[this.tabSelectedIndex];
     let latestVersions = {};
@@ -109,146 +92,6 @@ export class VersionPanelComponent implements OnInit {
     );
   }
 
-  onDescription(binary: Binary) {
-    this.dialog.open(TextDialogComponent, {
-      data: {
-        title: "更新日志",
-        content: binary.description,
-        editable: this.hasEditPermission,
-      }
-    }).afterClosed().subscribe(
-      data => {
-        if (this.hasEditPermission && data && data.data != binary.description) {
-          const request = new UpdateBinaryRequest({
-            binary: new Binary({
-              name: binary.name,
-              description: data.data,
-            }),
-            updateMask: new FieldMask({
-              paths: ['description']
-            })
-          });
-
-          this.san11pkService.updateBinary(request).subscribe(
-            binary => {
-              this.notificationService.success('更新成功');
-            },
-            error => {
-              this.notificationService.warn(`更新失败: ${error.statusMessage}`)
-            }
-          );
-        }
-      }
-    );
-  }
-
-  onDownload(binary: Binary) {
-    if (!signedIn() && getCategoryId(this.package.name) === 3) {
-      this.notificationService.warn('请登录');
-      return;
-    }
-
-    if (this.downloadSub != undefined) {
-      this.notificationService.warn(`下载中 ${this.downloadProgress}%... `);
-      return;
-    }
-    this.downloadProgress = 0;
-    this.downloadProgressBar = true;
-    this.binaryOnDownload = binary;
-    this.san11pkService.downloadBinary(new DownloadBinaryRequest({
-      name: this.binaryOnDownload.name
-    })).subscribe(
-      binary => {
-        const fileUrl = GlobalConstants.fileServerUrl + '/' + binary.file.uri;
-        const filename = getBinaryFilename(this.package, binary);
-
-        this.prevTime = new Date().getTime();
-        this.oldbytes = 0;
-
-        this.downloadSub = this.downloads.download(fileUrl, filename).subscribe(
-          result => {
-            if (result.type === HttpEventType.DownloadProgress) {
-              const percentDone = Math.round(100 * result.loaded / result.total);
-              this.downloadProgress = percentDone;
-
-              this.bytesReceied = result.loaded / 1000000;
-              this.currTime = new Date().getTime();
-              let speed =
-                (this.bytesReceied - this.oldbytes) /
-                ((this.currTime - this.prevTime) / 1000); // mb/second
-              if (speed < 1) {
-                this.unit = "KB/S";
-                speed *= 1000;
-              } else this.unit = "MB/S";
-
-              // Use Math.max(this.speed, 1) to handle this.speend == 0
-              if (Math.abs(this.speed - speed) / Math.max(this.speed, 1) > this.SPEED_UPDATE_DELTA_RATE) {
-                this.speed = speed;
-              }
-              this.prevTime = this.currTime;
-              this.oldbytes = this.bytesReceied;
-
-            }
-            if (result.type === HttpEventType.Response) {
-              this.downloadSub = undefined;
-
-              saveAs(result.body, filename);
-              this.downloadEvent.emit();
-              this.binaryOnDownload.downloadCount = increment(this.binaryOnDownload.downloadCount);
-
-              setTimeout(() => {
-                this.downloadProgressBar = false;
-              }, 5000);
-            }
-          },
-          error => {
-            this.downloadSub = undefined;
-            this.notificationService.warn('下载失败: ' + error.name);
-          }
-        );
-      },
-      error => {
-        this.notificationService.warn('下载失败' + error.statusMessage);
-      }
-    );
-  }
-
-  onDownloadMethod(binary: Binary) {
-    this.dialog.open(TextDialogComponent, {
-      data: {
-        title: '下载方式',
-        content: binary.downloadMethod
-      }
-    });
-    binary.downloadCount = increment(binary.downloadCount);
-    this.downloadEvent.emit();
-    this.san11pkService.downloadBinary(new DownloadBinaryRequest({
-      name: binary.name
-    })).subscribe();
-  }
-
-  onOffload(binary: Binary) {
-    if (!confirm('确定要 ' + version2str(binary.version) + ' 的文件资源吗? (只清除资源，版本将会保留)')) {
-      return;
-    }
-    this.san11pkService.updateBinary(new UpdateBinaryRequest({
-      binary: new Binary({
-        name: binary.name,
-      }),
-      updateMask: new FieldMask({
-        paths: ['file']
-      })
-    })).subscribe(
-      resp => {
-        this.notificationService.success('卸载成功');
-        this.loadBinaries();
-      },
-      error => {
-        this.notificationService.warn('卸载失败:' + error.statusMessage);
-      }
-    );
-  }
-
   onDelete(binary: Binary) {
     if (!confirm('确定要删除 ' + version2str(binary.version) + ' 吗?')) {
       return;
@@ -269,6 +112,20 @@ export class VersionPanelComponent implements OnInit {
 
   get hasEditPermission() {
     return isAdmin() || this.package.authorId === localStorage.getItem('userId');
+  }
+
+
+  updateDownloadProgress(event) {
+    this.downloadProgressBar = true;
+    this.downloadProgress = event.progress;
+    this.speed = event.speed;
+    this.unit = event.unit;
+
+    if (event.progress === 100) {
+      setTimeout(() => {
+        this.downloadProgressBar = false;
+      }, 5000);
+    }
   }
 }
 
