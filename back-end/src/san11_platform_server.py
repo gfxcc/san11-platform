d706175a04c4db72b9fe72fd743df52bff0b3cf6
Yong Cao <ycao181@gmail.com>
2023-11-04 23:46:45 -0700

    remove debugging msg

--- back-end/src/san11_platform_server.py
+++ back-end/src/san11_platform_server.py
@@ -152,12 +152,10 @@ class RouteGuideServicer(san11_platform_pb2_grpc.RouteGuideServicer):
             request=request,
             handler_context=context,
         )
-        ret = pb.ListBinariesResponse(
+        return pb.ListBinariesResponse(
             binaries=[binary.to_pb() for binary in binaries],
             next_page_token=next_page_token,
         )
-        logger.debug(ret.binaries)
-        return ret
 
     @GrpcAbortOnExcep
     @iam_util.assert_resource_owner('binary.name')
