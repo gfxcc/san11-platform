19b9b1d92e13954bcca2e3787216d2d383774084
Yong Cao <ycao181@gmail.com>
2024-11-25 14:13:48 -0800

    fix docker config issue which cause build failure related to node_modules

--- front-end/src/polyfills.ts
+++ front-end/src/polyfills.ts
@@ -58,7 +58,7 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'core-js/es/object';
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
 (window as any).global = window;
 
